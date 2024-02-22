ab07755675605cc395faab0563e26b92346bf2b1
Syed Hammad Ullah <syedhammadullah660@gmail.com>
2024-02-23 00:16:01 +0500

    Making Prompt Calculator

---
+++ app.js
@@ -0,0 +1,34 @@
+// Variables
+let myname = "Syed Hammad Ullah";
+let myage = 18;
+
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+
+console.log(myage);
+console.log(myage);
+console.log(myage);
+console.log(myage);
+
+let number1 = prompt("enter first number");
+let number2 = prompt("enter second number");
+// string Change into number
+number1 = Number(number1);
+number2 = Number(number2);
+
+console.log("Entered number1: ", number1);
+console.log("Entered number2: ", number2);
+console.log("Result Add: ", number1 + number2);
+console.log("Result Subtract: ", number1 - number2);
+console.log("Result Multiply: ", number1 * number2);
+console.log("Result Divide: ", number1 / number2);
+
+let sum = number1 + number2;
+let minus = number1 - number2;
+// Alert
+alert("Addition " + sum);
+alert("Subtraction " + minus);
+alert("Multiplication " + number1 * number2);
+alert("Division " + number1 / number2);
